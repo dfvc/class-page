bbfc8dcf5015c41cbb05aa9d556d19c453d9c363
Daniel Coelho <daniel.coelho@turbinekreuzberg.com>
2020-04-13 23:58:11 +0100

    Add tests for CpHeader and CpFooter components

--- src/components/TheFooter/__tests__/TheFooter.test.ts
+++ src/components/TheFooter/__tests__/TheFooter.test.ts
@@ -1,9 +1,26 @@
-import { shallowMount } from '@vue/test-utils';
+import {
+  shallowMount,
+  Wrapper,
+} from '@vue/test-utils';
 import CpFooter from '@/components/TheFooter/TheFooter.component.vue';
 
-describe('CpFooter component', () => {
-  test('mount without errors', () => {
-    const wrapper = shallowMount(CpFooter);
-    expect(wrapper.isVueInstance()).toBeTruthy();
+describe('CpFooter', () => {
+  let wrapper: Wrapper<CpFooter>;
+
+  beforeEach((): void => {
+    wrapper = shallowMount(CpFooter);
+  });
+
+  afterEach(() => {
+    wrapper.destroy();
+  });
+
+  /**
+   * General
+   */
+  describe('General', () => {
+    test('Component mounts without errors', () => {
+      expect(wrapper.isVueInstance()).toBeTruthy();
+    });
   });
 });
--- src/components/TheHeader/__tests__/TheHeader.test.ts
+++ src/components/TheHeader/__tests__/TheHeader.test.ts
@@ -22,6 +22,7 @@ describe('CpHeader', () => {
 
   afterEach(() => {
     wrapper.destroy();
+    jest.clearAllMocks();
   });
 
   /**
@@ -30,6 +31,64 @@ describe('CpHeader', () => {
   describe('General', () => {
     test('Component mounts without errors', () => {
       expect(wrapper.isVueInstance()).toBeTruthy();
+    });
+  });
+
+  /**
+   * Computed Properties
+   */
+  describe('Computed Properties', () => {
+    test.each`
+      scrollTopPosition | verboseScrollTopPosition  | expected
+      ${0}              | ${'0'}                    | ${false}
+      ${100}            | ${'> 0'}                  | ${true}
+    `(
+      '"hasShadow" returns $expected when scrollTopPosition is $verboseScrollTopPosition',
+      ({ scrollTopPosition, expected }) => {
+        wrapper.setData({
+          scrollTopPosition,
+        });
+        expect((wrapper.vm as any).hasShadow).toBe(expected);
+      },
+    );
+  });
+
+  /**
+   * Events
+   */
+  describe('Events', () => {
+    test('scroll event listener is added to the window when mounting the component', () => {
+      window.addEventListener = jest.fn();
+
+      expect(window.addEventListener).toHaveBeenCalledTimes(0);
+
+      wrapper.destroy();
+      wrapper = shallowMount(CpHeader, {
+        propsData: {
+          navigationItems,
+          title,
+        },
+      });
+
+      expect(window.addEventListener).toHaveBeenCalledTimes(1);
+      expect(window.addEventListener).toHaveBeenCalledWith(
+        'scroll',
+        (wrapper.vm as any).updateScrollTopPosition,
+      );
+    });
+  });
+
+  /**
+   * Methods
+   */
+  describe('Methods', () => {
+    test('"updateScrollTopPosition" set scrollTopPosition with document scrollTop value', () => {
+      const scrollTopMock = 100;
+      document.documentElement.scrollTop = scrollTopMock;
+
+      expect((wrapper.vm as any).scrollTopPosition).toEqual(0);
+      (wrapper.vm as any).updateScrollTopPosition();
+      expect((wrapper.vm as any).scrollTopPosition).toEqual(scrollTopMock);
     });
   });
 });
